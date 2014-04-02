bfc0faa289e845975fe0c1fd3b990258a3d04cf5
Chris Canipe <chris.canipe@wsj.com>
2014-04-01 22:27:53 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+/**
+ * @author Chris Canipe
+ */
+
+
+/*
+ * This is an array of all of our images (in order)*/
+var images = ["drain_0m.png", "drain_50m.png", "drain_100m.png", "drain_200m.png", "drain_500m.png", "drain_1km.png", "drain_2km.png", "drain_3km.png", "drain_5km.png", "drain_ed.png", "drain_nl.png"];
+
+/* Index of the current image in our images array */
+var index = 0;
+
+/* set button events here */
+function setNav() {
+	console.log("setNav");
+	
+	$(".button").on("click", function() {
+
+		var isNext = $(this).hasClass("next");
+		
+		if (isNext == true && index != (images.length-1)) {
+			index = index + 1;
+		} else if (isNext == false && index > 0) {
+			index = index - 1;
+		}
+		
+		
+		
+		
+		if (index == 0) {
+			$(".button.prev").addClass("disabled");
+		} else if (index == (images.length-1)) {
+			$(".button.next").addClass("disabled");
+		} else {
+			$(".button").removeClass("disabled");
+		}
+		
+		
+		updateImage();
+		
+	});
+	
+	
+}
+
+/* change the image here */
+function updateImage() {
+	$(".image-holder").html(
+		"<img src='images/"+images[index]+"'/>"
+	);
+}
+
+
+
+/* Load the first image into our image holder */
+$(document).ready(function() {
+	$(".image-holder").html(
+		"<img src='images/"+images[index]+"'/>"
+	);
+	
+	$(".button.prev").addClass("disabled");
+	
+	setNav();
+	
+});
+
